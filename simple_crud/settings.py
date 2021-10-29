cf4e90aa3b020be2760eca0ab1907ee049013c8e
97wave <81473160+97wave@users.noreply.github.com>
2021-10-29 19:50:20 +0300

    гзв

--- simple_crud/settings.py
+++ simple_crud/settings.py
@@ -133,15 +133,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+# STATIC_URL = '/static/'
+# STATIC_ROOT = BASE_DIR / 'static'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'assets',
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'assets',
+# ]
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = BASE_DIR / 'media'
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
