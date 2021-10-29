8abdaf9e7facd56db1d7280bd9e2506baa7a06c3
97wave <e97.wav@gmail.com>
2021-10-29 19:23:05 +0300

    3

--- simple_crud/settings.py
+++ simple_crud/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'j$1(ig@@#+2-ismgv-!xayzd^g*9ryzf_h(pfk6%p18-5$yrl^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
